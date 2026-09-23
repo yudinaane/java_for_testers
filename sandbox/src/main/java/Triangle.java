fded3fb932c69ab207710de2c79a87916f68b28e
anna.yudina <yudinaane@gmail.com>
2026-09-23 16:53:20 +0300

    Тесты для Triangle

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -3,26 +3,26 @@ import static java.lang.Math.sqrt;
 public class Triangle {
 
     public static void main(String[] args) {
-        printPerimPerimetrTriangle(5.,4.,3);
-        printSquareTriangle (5.,4.,3.);
+        printPerimetrTriangle(5,4,3);
+        printSquareTriangle (5,4,3);
            }
 
-    private static void printPerimPerimetrTriangle(double a, double b, double c) {
-        String text = String.format("Периметр треугольника со сторонами %f, %f и %f равен %f", a, b, c, calculatePerimetrTriangle(a, b, c) );
+    public static void printPerimetrTriangle(double a, double b, double c) {
+        String text = String.format("Периметр треугольника со сторонами %f, %f и %f равен %f", a, b, c, calculatePerimeterTriangle(a, b, c) );
         System.out.println(text);
     }
-    private static double calculatePerimetrTriangle(double a, double b, double c) {
+    public static double calculatePerimeterTriangle(double a, double b, double c) {
         return a + b + c;
     }
-    private static void printSquareTriangle(double a, double b, double c) {
+    public static void printSquareTriangle(double a, double b, double c) {
         String text = String.format("Площадь треугольника по формуле Герона c полупериметром %f и сторонами %f, %f и %f равен %f",
-                calculatePerimetrTriangle(a, b, c)/2, a, b, c, calculateSquareTriangle(a, b, c));
+                calculatePerimeterTriangle(a, b, c)/2, a, b, c, calculateSquareTriangle(a, b, c));
         System.out.println(text);
     }
 
-    private static double calculateSquareTriangle(double a, double b, double c) {
-        var semiPerimetr=calculatePerimetrTriangle(a, b, c)/2;
-        return sqrt(semiPerimetr*(semiPerimetr-a)*(semiPerimetr-b)*(semiPerimetr-c));
+    public static double calculateSquareTriangle(double a, double b, double c) {
+        var semiPerimeter = calculatePerimeterTriangle(a, b, c)/2;
+        return sqrt(semiPerimeter *(semiPerimeter-a)*(semiPerimeter-b)*(semiPerimeter-c));
     }
 
 }
---
+++ sandbox/src/test/java/TriangleTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTests {
+    @Test
+    void canCalculatePerimeterTriangle(){
+        Assertions.assertEquals(12, Triangle.calculatePerimeterTriangle(5, 4, 3));
+    }
+    @Test
+    void canCalculateSquareTriangle(){
+        Assertions.assertEquals(6, Triangle.calculateSquareTriangle(5, 4, 3));
+    }
+
+}
