ba4ffc33d248d24f9ce1b2561052719e74f6890e
anna.yudina <yudinaane@gmail.com>
2026-09-23 14:03:40 +0300

    Изменены функции вывода результатов

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -8,14 +8,16 @@ public class Triangle {
            }
 
     private static void printPerimPerimetrTriangle(double a, double b, double c) {
-        System.out.println("Периметр треугольника со сторонами " + a + ", " + b + ", " + c + " равен " + calculatePerimetrTriangle(a, b, c));
+        String text = String.format("Периметр треугольника со сторонами %f, %f и %f равен %f", a, b, c, calculatePerimetrTriangle(a, b, c) );
+        System.out.println(text);
     }
-
     private static double calculatePerimetrTriangle(double a, double b, double c) {
         return a + b + c;
     }
     private static void printSquareTriangle(double a, double b, double c) {
-        System.out.println("Площадь треугольника по формуле Герона c полупериметром " + calculatePerimetrTriangle(a, b, c)/2 + " и сторонами " + a + ", " + b + ", " + c + " равен " + calculateSquareTriangle(a, b, c));
+        String text = String.format("Площадь треугольника по формуле Герона c полупериметром %f и сторонами %f, %f и %f равен %f",
+                calculatePerimetrTriangle(a, b, c)/2, a, b, c, calculateSquareTriangle(a, b, c));
+        System.out.println(text);
     }
 
     private static double calculateSquareTriangle(double a, double b, double c) {
